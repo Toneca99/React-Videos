15ae17cd94755a73ea9edf5c0d9351aefc8b6503
Anton Lafchiev <44170834+Toneca99@users.noreply.github.com>
2019-08-16 01:52:39 +0300

    Updates

---
+++ precache-manifest.87b707f4d4e02fff286f1cfa6f143a5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2cbdd629d77d742e4fb79a63377c627",
+    "url": "/Toneca99/React-Videos.git/index.html"
+  },
+  {
+    "revision": "b4e940a1348903bef02c",
+    "url": "/Toneca99/React-Videos.git/static/css/main.d3ee3a7a.chunk.css"
+  },
+  {
+    "revision": "9e67e8b0a6bbafd523d8",
+    "url": "/Toneca99/React-Videos.git/static/js/2.e28db64f.chunk.js"
+  },
+  {
+    "revision": "b4e940a1348903bef02c",
+    "url": "/Toneca99/React-Videos.git/static/js/main.25dfc02f.chunk.js"
+  },
+  {
+    "revision": "91332e7abebcdc25aa11",
+    "url": "/Toneca99/React-Videos.git/static/js/runtime~main.7aa577da.js"
+  }
+]);
